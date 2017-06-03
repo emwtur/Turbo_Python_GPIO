d738b17f90b62dbed5ae9656fb87916d6b0f9589
emwtur <emwtur@gmail.com>
2017-06-04 00:38:00 +0200

    missing file

---
+++ examples/HCSR04_loop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import logging
+import time
+logging.basicConfig(level=logging.DEBUG)
+import Turbo_GPIO.HCSR04 as HCSR04
+
+
+sensor = HCSR04.HCSR04()
+
+while True:
+	distance = sensor.read_distance()
+	print 'Distance      = {0:0.2f} cm'.format(distance)
+	time.sleep(2)
+
+
